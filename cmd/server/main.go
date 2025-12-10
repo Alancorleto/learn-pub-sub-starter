d6d71fb16408a4c33a910a1e48f2b89032f7f452
alancorleto <corletoalan@gmail.com>
2025-12-10 17:55:12 -0300

    Configure server to run RabbitMQ container

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatal("Unable to start amqp connection")
+	}
+	defer connection.Close()
+	fmt.Println("Connection succesfull!")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Shutting down server...")
 }
