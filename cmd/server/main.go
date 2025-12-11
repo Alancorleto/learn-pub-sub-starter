d694cba897ba4596d278da0a10523dfc2cf05703
alancorleto <corletoalan@gmail.com>
2025-12-11 16:33:38 -0300

    Build a REPL for the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 
@@ -22,12 +21,49 @@ func main() {
 	defer connection.Close()
 	fmt.Println("Connection succesfull!")
 
+	gamelogic.PrintServerHelp()
+
 	channel, err := connection.Channel()
 	if err != nil {
 		log.Fatal("Unable to create connection channel: ", err)
 	}
 
-	pubsub.PublishJSON(
+	for {
+		inputs := gamelogic.GetInput()
+		if len(inputs) == 0 {
+			continue
+		}
+		input := inputs[0]
+
+		if input == "pause" {
+			fmt.Println("Sending pause message...")
+			err = sendPauseMessage(channel)
+			if err != nil {
+				log.Println(err)
+			}
+		} else if input == "resume" {
+			fmt.Println("Sending resume message...")
+			err = sendResumeMessage(channel)
+			if err != nil {
+				log.Println(err)
+			}
+		} else if input == "quit" {
+			fmt.Println("Exiting...")
+			break
+		} else {
+			fmt.Println("Unrecognized command")
+		}
+	}
+
+	// signalChan := make(chan os.Signal, 1)
+	// signal.Notify(signalChan, os.Interrupt)
+	// <-signalChan
+
+	// fmt.Println("Shutting down server...")
+}
+
+func sendPauseMessage(channel *amqp.Channel) error {
+	return pubsub.PublishJSON(
 		channel,
 		routing.ExchangePerilDirect,
 		routing.PauseKey,
@@ -35,10 +71,15 @@ func main() {
 			IsPaused: true,
 		},
 	)
+}
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-
-	fmt.Println("Shutting down server...")
+func sendResumeMessage(channel *amqp.Channel) error {
+	return pubsub.PublishJSON(
+		channel,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: false,
+		},
+	)
 }
